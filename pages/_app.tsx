eeb34fc2bc5422e3e289b8569ed4e78870494c45
Liam Arbuckle <liam@skinetics.tech>
2023-03-08 18:20:20 +1100

    ⛈🏳️‍🌈 ↝ Passion hits the street. Welcome to DFRNTL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import NavBarSide from '@/components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='flex flex-row w-screen h-screen'>
+      <NavBarSide/>
+      <Component {...pageProps} />
+    </div>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'primary': '#dbb381',
+      'black':'#ffffff',
+      'white':'#000000',
+      'blue':'#b9e1ec',
+      'green':'#d6db81'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
